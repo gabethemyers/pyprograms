db22c0612f07c097d8b539620648614c6b55e0dc
gabethemyers <gabethemyers@gmail.com>
2022-05-30 12:14:39 -0700

    made changes

--- destiny2_lostsector/bing.py
+++ destiny2_lostsector/bing.py
@@ -1,12 +1,13 @@
 import time
 import smtplib, ssl
 from openpyxl import load_workbook
+import illegal_stuff
 
 def emailSend(receiver_email, message):
     context = ssl.create_default_context()
     with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
-        server.login("tt2249110@gmail.com", 'grac1e11')
-        server.sendmail("tt2249110@gmail.com", receiver_email, message)
+        server.login(illegal_stuff.email_username, illegal_stuff.email_pass)
+        server.sendmail(illegal_stuff.email_username, receiver_email, message)
 
 # intitalizing workbook and sheet, also getting todays date.
 workbook = load_workbook(filename='Lost Sector.xlsx')
@@ -26,8 +27,8 @@ lost_sector_name = sheet.cell(row=index, column=3).value
 master_lost_sector_loot = sheet.cell(row=index, column=6).value
 
 if lost_sector_name == 'Chamber of Starlight (DC)':
-    emailSend('5307444408@vtext.com', 'Today\'s lost sector is Chamber of Starlight (DC)')
+    emailSend(illegal_stuff.my_email, 'Today\'s lost sector is Chamber of Starlight (DC)')
 if lost_sector_name == 'Perdition (Europa)':
-    emailSend('5307444408@vtext.com', 'Today\'s lost sector is Perdition (Europa)')
+    emailSend(illegal_stuff.my_email, 'Today\'s lost sector is Perdition (Europa)')
 if master_lost_sector_loot == 'Legs':
-    emailSend('5307444408@vtext.com', 'Today\'s master lost sector loot is legs')
+    emailSend(illegal_stuff.my_email, 'Today\'s master lost sector loot is legs')
